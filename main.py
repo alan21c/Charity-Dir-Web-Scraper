c210b044da7adf5718ab5e4e4ae584908d6e2def
Alan Li <lixx4118@mylaurier.ca>
2020-08-15 21:28:08 -0400

    Skeleton CSV writing functionality for Adam to flesh out.

--- main.py
+++ main.py
@@ -1,2 +1,22 @@
-print("Hello World!")
-input("Press enter to continue:")
+import csv
+
+# function skeleton to be fleshed out
+def arrayToCSV(filename, array):
+	return
+
+# test data
+sampleArray = [[1, 2, 3], [3, 2, 1], [4, 5, 6]]
+arrayToCSV("output.csv", sampleArray)
+
+# sample CSV writing code
+with open('employee_file2.csv', mode='w') as csv_file:
+    fieldnames = ['emp_name', 'dept', 'birth_month']
+    writer = csv.DictWriter(csv_file, fieldnames=fieldnames)
+
+    writer.writeheader()
+    writer.writerow({'emp_name': 'John Smith', 'dept': 'Accounting', 'birth_month': 'November'})
+    writer.writerow({'emp_name': 'Erica Meyers', 'dept': 'IT', 'birth_month': 'March'})
+	
+# finished program
+print("main.py finished running")
+input()
