a150bcc9a6810b10707b3f784e41c7c5c9dddaeb
BarnZarn <just.adamzheng@gmail.com>
2020-08-16 15:28:55 -0700

    updated function writing to excel

--- main.py
+++ main.py
@@ -2,7 +2,14 @@ import csv
 
 # function skeleton to be fleshed out
 def arrayToCSV(filename, array):
-	return
+        f = open(filename, "w")
+        for i in range(len(array)):
+                for j in range(len(array[i])):
+                        f.write(str(array[i][j]))
+                        f.write(",")
+                f.write("\n")
+        f.close()
+        return
 
 # test data
 sampleArray = [[1, 2, 3], [3, 2, 1], [4, 5, 6]]
