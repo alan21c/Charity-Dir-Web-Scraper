ce4b09c52b575bd892c784717a9ad39b1cd78067
Alan Li <lixx4118@mylaurier.ca>
2020-08-25 19:38:43 -0400

    Added timer.

--- main.py
+++ main.py
@@ -1,20 +1,50 @@
-import csv
+import urllib.request
+import re
+import time
+import json
+charities = open("charities.csv", "w")
+charities.write("Contact Name, Email, Phone Number, Website, Charity Name, Revenue\n")
+temprow = []
+#3458
 
-# function skeleton to be fleshed out
-def arrayToCSV(filename, array):
-        f = open(filename, "w")
-        for i in range(len(array)):
-                for j in range(len(array[i])):
-                        f.write(str(array[i][j]))
-                        f.write(",")
-                f.write("\n")
-        f.close()
-        return
+start = time.perf_counter()
 
-# test data
-sampleArray = [[1, 2, 3], [3, 2, 1], [4, 5, 6]]
-arrayToCSV("output.csv", sampleArray)
-	
-# finished program
-print("main.py finished running")
-input()
+for i in range(1):
+    
+    #What this does is open a database with the Buisness Numbers of each charity (alongside other stuff) according to the page number, i. We modify the url based on i.
+    links = json.loads(urllib.request.urlopen(f"https://www.charitydir.com/views_2018_08_05/charities/getCharities.php?name=undefined&page={i + 1}&all=true").read())
+    BN = []
+    
+    for row in links["charities"]:
+        BN.append(row["BN"])
+    
+    print("Read all BNs. Time elapsed: %s", (time.perf_counter() - start))
+    
+    for bn in BN:
+        #Opens data for specific CSV file
+        data = json.loads(urllib.request.urlopen(f"https://www.charitydir.com/views_2018_08_05/charity/getCharityBasic.php?BN={bn}").read())["charity"]
+        
+        print("Read Charity JSON. Time elapsed: %s", (time.perf_counter() - start))
+        
+        #Adds everything
+        temprow = [data["PublicContactName"], data["Email"], data["ContactPhoneNumber"], data["Website"], data["LegalName"]]
+
+        #Revenue is stored in a different php file
+        data = json.loads(urllib.request.urlopen(f"https://www.charitydir.com/views_2018_08_05/charity/getCharity.php?BN={bn}").read())["charity"]["financial"]
+        
+        print("Read Charity finances JSON. Time elapsed: %s", (time.perf_counter() - start))
+        
+        #Get Revenue
+        temprow.append(data["4700"])
+        #Writes it to CSV
+        if(float(temprow[5]) > 200000):
+            for cell in temprow:
+                if cell == None:
+                    charities.write(",")
+                else:
+                    charities.write('"'+cell+'",')
+            charities.write('\n')
+            
+    print("Wrote to CSV. Time elapsed: %s", (time.perf_counter() - start))    
+
+charities.close()
--- test.py
+++
@@ -1,7 +0,0 @@
-from lib.requests_html import HTMLSession
-
-session = HTMLSession()
-r = session.get('https://www.charitydir.com/all/1')
-r.html.render()
-
-print(r.html.links)
